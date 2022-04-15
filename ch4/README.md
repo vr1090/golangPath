ec5d9f2ed088dd2bd77604f55c3e99b651420ea9
inu <vr1090@gmail.com>
2022-04-15 19:24:14 +0700

    add struct

--- ch4/README.md
+++ ch4/README.md
@@ -2,3 +2,8 @@
 
 ## array
 - size bagian dari tipe array
+
+## slice
+- an array without a size
+- contains: a pointer, a size , a capacity
+- 
---
+++ go programming.mm
@@ -0,0 +1,8 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1650023894298" ID="ID_1098023256" MODIFIED="1650023900904" TEXT="go programming">
+<node CREATED="1650023901250" ID="ID_1588879175" MODIFIED="1650023904149" POSITION="right" TEXT="struct">
+<node CREATED="1650023904423" ID="ID_1426646535" MODIFIED="1650023908438" TEXT="dalemnya field"/>
+</node>
+</node>
+</map>
